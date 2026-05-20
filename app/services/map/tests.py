554b60c15c1406f3c36af98ff3191f79b4ac03ab
tulolo287 <iv1055@yandex.ru>
2026-05-20 19:11:51 +0300

    asfsaf

--- app/services/map/tests.py
+++ app/services/map/tests.py
@@ -1,9 +1,11 @@
 from http import HTTPStatus
 from unittest.mock import AsyncMock, patch
+
 from django.test import RequestFactory, TransactionTestCase, override_settings
 from django.urls import reverse
 from inertia.test import InertiaTestCase
 
+from app.services.map.views import MapListView
 from app.services.vacancies.models import Vacancy
 from app.services.vacancies.tests.factories import VacancyFactory
 
@@ -17,18 +19,11 @@ class MapTests(TransactionTestCase):
             region=cls.vacancies_data["region"],
         )
 
-    @patch("app.services.vacancies.views.inertia_render")
-    def test_map_list_view_get(self, mock_inertia_render):
-        response = self.client.get(reverse("map_list"))
-        props = self.props()
-        map_data = props["mapData"]
-        region_data = map_data[0]
+    async def test_map_list_view_get(self):
+        # response = self.client.get(reverse("map_list"))
+        request = self.factory.get("/map/")
+        view = MapListView()
+        response = await view.get(request)
 
-        self.assertIn("mapData", props)
-        self.assertEqual(len(map_data), 1)
-        self.assertComponentUsed("MapPage")
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
-        self.assertEqual(region_data["region"], self.vacancies_data["region"])
-        self.assertEqual(
-            region_data["totalVacancies"], self.vacancies_data["totalVacancies"]
-        )
+        self.assertIn(response, self.vacancies_data)
