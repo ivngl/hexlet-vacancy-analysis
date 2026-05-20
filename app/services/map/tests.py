3b6a2bb4f0eb22aed1d240a0b16dda7a7c39d2d4
tulolo287 <iv1055@yandex.ru>
2026-05-20 23:51:13 +0300

    afddaadf

--- app/services/map/tests.py
+++ app/services/map/tests.py
@@ -1,29 +1,25 @@
 from http import HTTPStatus
-from unittest.mock import AsyncMock, patch
 
-from django.test import RequestFactory, TransactionTestCase, override_settings
+from django.test import TransactionTestCase
 from django.urls import reverse
-from inertia.test import InertiaTestCase
 
-from app.services.map.views import MapListView
-from app.services.vacancies.models import Vacancy
 from app.services.vacancies.tests.factories import VacancyFactory
 
 
 class MapTests(TransactionTestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.vacancies_data = {"totalVacancies": 5, "region": "Moscow"}
-        VacancyFactory.create_batch(
-            cls.vacancies_data["totalVacancies"],
-            region=cls.vacancies_data["region"],
-        )
+    def setUp(self):
+        self.vacancies_data = [{"totalVacancies": 5, "region": "Moscow"}]
 
-    async def test_map_list_view_get(self):
-        # response = self.client.get(reverse("map_list"))
-        request = self.factory.get("/map/")
-        view = MapListView()
-        response = await view.get(request)
+        for v in self.vacancies_data:
+            VacancyFactory.create_batch(
+                v["totalVacancies"],
+                region=v["region"],
+            )
+
+    def test_map_list_view_get(self):
+        response = self.client.get(reverse("map_list"))
+        data = response.props["mapData"][0]
 
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
-        self.assertIn(response, self.vacancies_data)
+        for v in self.vacancies_data:
+            self.assertDictEqual(data, v)
